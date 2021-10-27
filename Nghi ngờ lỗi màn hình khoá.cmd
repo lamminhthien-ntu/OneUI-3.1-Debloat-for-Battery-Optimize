f976d412b5b822b924fbaac6d7fe033a1851e392
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2021-10-27 07:13:10 -0700

    Create Nghi ngờ lỗi màn hình khoá.cmd

---
+++ Nghi ngờ lỗi màn hình khoá.cmd
@@ -0,0 +1,7 @@
+shell pm uninstall --user 0 com.samsung.android.accessibility.talkback
+Success
+
+shell pm uninstall --user 0 com.samsung.android.aircommandmanager
+Success
+
+shell pm uninstall --user 0 com.samsung.android.app.aodservice
